fe5f31e3df87832a5097977dffacae87930ddf82
kofston <lukkonop@icloud.com>
2022-04-10 18:35:10 +0200

    Add history api NBP call (pass test)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -40,4 +40,24 @@ class MainController extends Controller
             return 0;
     }
 
+    public function get_api_history()
+    {
+            $today = date('Y-m-d');
+            $month_before = date('Y-m-d',strtotime('- 1 month'));
+
+            $ch = curl_init("http://api.nbp.pl/api/exchangerates/tables/A/".$month_before."/".$today."");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            $data = curl_exec($ch);
+            curl_close($ch);
+            try {
+                $decode = json_decode($data);
+                return $decode;
+            }
+            catch (Exception $e)
+            {
+                return 0;
+            }
+
+    }
 }
