0e4345d3cecf18a8c65af5c4833a16ad8af2b653
kofston <lukkonop@icloud.com>
2022-04-11 20:51:24 +0200

    Add frontend to toword function

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,6 +56,9 @@
             <div id="response">
                 <h5>Kliknij przelicz aby przeliczyć kurs</h5>
             </div>
+            <div>
+                <table style="width: 100%;"><tr><td id="word_usd"></td><td id="word_eur"></td><td id="word_gbp"></td></tr></table>
+            </div>
             <div id="response_history">
                 <h5>Historia USD:</h5>
                 <div id="chart_usd" style="height: 200px;width: 100%;"></div>
@@ -110,7 +113,39 @@ function drawChart(CHAR_usd,CHAR_eur,CHAR_funt)
                             console.log(result[0]);
                             let usd = result[0].rates[1].mid,eur = result[0].rates[7].mid,funt = result[0].rates[10].mid;
                             let finish_usd = (parseFloat(val)/parseFloat(usd)).toFixed(2),finish_euro = (parseFloat(val)/parseFloat(eur)).toFixed(2),finish_funt = (parseFloat(val)/parseFloat(funt)).toFixed(2);
+                            let word_usd = '',word_eur = '',word_funt='';
                             $("#response").html('<table width="100%"style="border:1pxsolidblack;"><tr><th style="border:1pxsolidblack;"class="text-center"><img width="100px"src="https://cdn-icons-png.flaticon.com/512/555/555526.png"/><h3>Dolar</h3></th><th style="border:1pxsolidblack;"class="text-center"><img width="100px"src="https://www.flagi-24.pl/images/panstwa/unia_europejska.png"/><h3>Euro</h3></th><th style="border:1pxsolidblack;"class="text-center"><img width="100px"src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/1024px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png"/><h3>Funt</h3></th></tr><tr><td style="border:1pxsolidblack;"colspan="3">Dane z dnia: '+day+'</td></tr><tr><td style="border:1pxsolidblack;"colspan="3">Średnikurs:</td></tr><tr><td style="border:1pxsolidblack;">'+usd+'</td><td style="border:1pxsolidblack;">'+eur+'</td><td style="border:1pxsolidblack;">'+funt+'</td></tr><tr><td style="border:1pxsolidblack;font-size:20px;"colspan="3">Wyliczenia:</td></tr><tr><td style="border:1pxsolidblack;font-size:22px;">'+finish_usd+' zł</td><td style="border:1pxsolidblack;font-size:22px;">'+finish_euro+' zł</td><td style="border:1pxsolidblack;font-size:22px;">'+finish_funt+' zł</td></tr></table>');
+
+                            for(var i=0;i<3;i++)
+                            {
+                                switch (i)
+                                {
+                                    case 0:
+                                        $.ajax({
+                                            url: "/toword/"+finish_usd,
+                                            success:function (result){
+                                                $("#word_usd").html("<strong>"+result+"</strong>");
+                                            }
+                                        });
+                                        break;
+                                    case 1:
+                                        $.ajax({
+                                            url: "/toword/"+finish_euro,
+                                            success:function (result){
+                                                $("#word_eur").html("<strong>"+result+"</strong>");
+                                            }
+                                        });
+                                        break;
+                                    case 2:
+                                        $.ajax({
+                                            url: "/toword/"+finish_funt,
+                                            success:function (result){
+                                                $("#word_gbp").html("<strong>"+result+"</strong>");
+                                            }
+                                        });
+                                        break;
+                                }
+                            }
                         }
                         else
                             alert("Wystąpił problem z API");
